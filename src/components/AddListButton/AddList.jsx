7fecf452e71b12c65f89634b442040c1c0e511af
Earn1ll <coldsteelblr@gmail.com>
2021-10-27 22:19:07 +0300

    update component addList again

--- src/components/AddListButton/AddList.jsx
+++ src/components/AddListButton/AddList.jsx
@@ -4,9 +4,11 @@ import addsvg from "../../assets/img/add.svg";
 import "./AddList.scss";
 import Badge from "../Badge/Badge";
 
+import closeSvg from "../../assets/img/close.svg";
+
 const AddList = ({colors}) => {
-    const [visiblePopup, setVisiblePopup] = useState(true);
-    const [selectedColor, selectColor] = useState(null);
+    const [visiblePopup, setVisiblePopup] = useState(false);
+    const [selectedColor, selectColor] = useState(colors[0].id);
     return (
         <div className="add_List">
             <List
@@ -20,6 +22,11 @@ const AddList = ({colors}) => {
                 ]}
             />
             {visiblePopup && <div className="add_list_popup">
+                <img
+                    onClick={() => setVisiblePopup(false)}
+                    src={closeSvg}
+                    alt='close button'
+                    className="add_list_popup_closeBtn"></img>
                 <input className="field" type="text" placeholder="Название списка"/>
                 <div className="add_list_popup_colors">
 
